cc87e55ca1985bca90e57e340bd40ae3b4096a45
Arcadiy Ivanov <arcadiy@ivanov.biz>
2023-07-01 15:07:59 -0400

    SubmoduleAwareBuilder has been fixed in tito 0.6.23 Removing local patch

--- .tito/libs/karellen/tito/__init__.py
+++ .tito/libs/karellen/tito/__init__.py
@@ -1,4 +1,3 @@
-from karellen.tito.submodule_aware_builder import SubmoduleAwareBuilder
 from karellen.tito.version_tagger import SysboxVersionTagger
 
-__all__ = ["SubmoduleAwareBuilder", "SysboxVersionTagger"]
+__all__ = ["SysboxVersionTagger"]
